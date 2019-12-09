456165f0bb1ae464aef3d6ce06f683dd23d4f810
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2019-12-09 20:34:42 +0000

    Refactor ✂️

--- src/uk/co/lewisodriscoll/aoc/day4.kt
+++ src/uk/co/lewisodriscoll/aoc/day4.kt
@@ -5,8 +5,6 @@ import kotlin.math.pow
 fun Int.pow(n: Int): Int = this.toDouble().pow(n).toInt()
 fun Int.digits(): List<Int> = this.toString().toList().map(Char::toString).map(String::toInt)
 
-fun digitsToNumber(digits: List<Int>): Int = digits.reduceRightIndexed { n, x, acc -> acc + 10.pow(digits.size - 1 - n) * x }
-
 fun countDigits(digits: List<Int>): HashMap<Int, Int> {
     val frequencies: HashMap<Int, Int> = hashMapOf()
     for (digit: Int in digits) {
@@ -20,41 +18,27 @@ fun countDigits(digits: List<Int>): HashMap<Int, Int> {
     return frequencies
 }
 
-fun contains2ConsecutiveDigits(digits: List<Int>): Boolean = countDigits(digits)
-    .values
-    .fold(false) { acc: Boolean, freq: Int ->
-        acc || (freq >= 2)
-    }
-
-fun containsPairOfDigits(digits: List<Int>): Boolean {
-    val frequencies: HashMap<Int, Int> = countDigits(digits)
-
-    return frequencies.values
-        .fold(false) { acc: Boolean, freq: Int ->
-            acc || (freq == 2)
-        }
-}
-
-fun part1(searchSpace: List<Int>): List<Int> = searchSpace
+fun part1(searchSpace: Iterable<Int>): Int = searchSpace
     .map(Int::digits)
-    .filter { digits: List<Int> -> digits.sorted() == digits }
-    .filter { digits: List<Int> -> contains2ConsecutiveDigits(digits) }
-    .map { digits: List<Int> -> digitsToNumber(digits) }
+    .filter { it.sorted() == it }
+    .map { countDigits(it).values }
+    .count { it.fold(false) { acc: Boolean, freq: Int -> acc || freq >= 2 } }
 
-fun part2(searchSpace: List<Int>): List<Int> = searchSpace
+fun part2(searchSpace: Iterable<Int>): Int = searchSpace
     .map(Int::digits)
-    .filter { digits: List<Int> -> containsPairOfDigits(digits) }
-    .map { digits: List<Int> -> digitsToNumber(digits) }
+    .filter { it.sorted() == it }
+    .map { countDigits(it).values }
+    .count { it.fold(false) { acc: Boolean, freq: Int -> acc || freq == 2 } }
 
 fun main() {
     val lowerBound: Int = 172930
     val upperBound: Int = 683082
 
-    val searchSpace = (lowerBound..upperBound).toList()
+    val searchSpace = lowerBound..upperBound
 
-    val validPasswords1: List<Int> = part1(searchSpace)
-    println("Answer 1: ${validPasswords1.size}")
+    val answer1: Int = part1(searchSpace)
+    println("Answer 1: $answer1")
 
-    val validPasswords2: List<Int> = part2(validPasswords1)
-    println("Answer 2: ${validPasswords2.size}")
+    val answer2: Int = part2(searchSpace)
+    println("Answer 2: $answer2")
 }
