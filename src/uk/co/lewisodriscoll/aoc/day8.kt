ad0f571d17554f957bcf05348be8ff3b5db40407
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2019-12-13 23:19:50 +0000

    Add day 8 pt 2 solution

--- src/uk/co/lewisodriscoll/aoc/day8.kt
+++ src/uk/co/lewisodriscoll/aoc/day8.kt
@@ -3,11 +3,17 @@ package uk.co.lewisodriscoll.aoc
 import uk.co.lewisodriscoll.aoc.util.readFile
 
 typealias Layer = List<List<Int>>
+fun Layer.print() = forEach {
+    it.forEach { pixel ->
+        if (pixel == 1) print("#")
+        else print(" ")
+    }
+    println()
+}
+
 typealias Image = List<Layer>
 
-fun toImage(input: String, width: Int, height: Int): Image = input
-    .map(Char::toString)
-    .map(String::toInt)
+fun toImage(input: List<Int>, width: Int, height: Int): Image = input
     .chunked(width * height)
     .map { it.chunked(width) }
 
@@ -20,12 +26,32 @@ fun part1(image: Image): Int {
     return countValues(winningLayer, 1) * countValues(winningLayer, 2)
 }
 
+fun part2(input: List<Int>, width: Int, height: Int): Layer {
+    val layers = input.chunked(width * height)
+    val output = MutableList(width * height) { 2 }
+    layers.forEach { layer ->
+        layer.forEachIndexed { index, pixel ->
+            val current: Int = output[index]
+            if (current == 2) output[index] = pixel
+        }
+    }
+
+    return output.chunked(width)
+}
+
 fun main() {
     val width: Int = 25
     val height: Int = 6
 
-    val input: String = readFile("day8.txt").first()
+    val input: List<Int> = readFile("day8.txt")
+        .first()
+        .map(Char::toString)
+        .map(String::toInt)
+
     val image: Image = toImage(input, width, height)
     val part1Answer: Int = part1(image)
     println("Answer (part 1): $part1Answer")
+
+    val outputImage = part2(input, width, height)
+    outputImage.print()
 }
