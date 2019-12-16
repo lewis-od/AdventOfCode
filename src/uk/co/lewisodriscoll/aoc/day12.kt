f90b9015090bd21ae51d7589406389652af32f61
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2019-12-16 21:40:41 +0000

    Make it functional

--- src/uk/co/lewisodriscoll/aoc/day12.kt
+++ src/uk/co/lewisodriscoll/aoc/day12.kt
@@ -2,67 +2,61 @@ package uk.co.lewisodriscoll.aoc
 
 import kotlin.math.abs
 
-class Vec3(var x: Int, var y: Int, var z: Int) {
+class Moon(val x: Int, val y: Int, val z: Int, val vx: Int, val vy: Int, val vz: Int) {
+    constructor(x: Int, y: Int, z: Int) : this(x, y, z, 0, 0, 0)
 
-    fun absolute(): Int = abs(x) + abs(y) + abs(z)
+    private fun getPotentialEnergy(): Int = abs(x) + abs(y) + abs(z)
 
-    operator fun plus(v: Vec3) = Vec3(x + v.x, y + v.y, z + v.z)
-
-    operator fun get(i: Int): Int = when (i) {
-        0 -> x
-        1 -> y
-        2 -> z
-        else -> throw IndexOutOfBoundsException("Expected $i <= 2")
-    }
-
-    operator fun set(i: Int, v: Int) = when (i) {
-        0 -> x = v
-        1 -> y = v
-        2 -> z = v
-        else -> throw IndexOutOfBoundsException("Expected $i <= 2")
-    }
-
-    override fun toString(): String {
-        return "<x=$x, y=$y, z=$z>"
-    }
-}
-
-class Moon(var position: Vec3) {
-    constructor(x: Int, y: Int, z: Int) : this(Vec3(x, y, z))
-    var velocity: Vec3 = Vec3(0, 0 ,0)
-
-    fun getPotentialEnergy(): Int = position.absolute()
-
-    fun getKineticEnergy(): Int = velocity.absolute()
+    private fun getKineticEnergy(): Int = abs(vx) + abs(vy) + abs(vz)
 
     fun getTotalEnergy(): Int = getPotentialEnergy() * getKineticEnergy()
 
-    override fun toString(): String = "position: $position, velocity: $velocity"
+    override fun toString(): String = "position: ($x, $y, $z), velocity: ($vx, $vy, $vz)"
 }
 
-fun updateVelocities(moons: List<Moon>) {
-    val n: Int = moons.size
-    for (i in 0 until n) {
-        for (j in (i + 1) until n) {
-            val moonA = moons[i]
-            val moonB = moons[j]
-            for (k in 0..2) {
-                val xa = moonA.position[k]
-                val xb = moonB.position[k]
+fun calculateVelocity(moon: Moon, other: Moon): Moon {
+    var dvx = 0
+    var dvy = 0
+    var dvz = 0
 
-                if (xa < xb) {
-                    moonA.velocity[k] += 1
-                    moonB.velocity[k] -= 1
-                } else if (xb < xa) {
-                    moonA.velocity[k] -= 1
-                    moonB.velocity[k] += 1
-                }
-            }
-        }
+    if (moon.x > other.x) {
+        dvx -= 1
+    } else if (moon.x < other.x) {
+        dvx += 1
     }
+
+    if (moon.y > other.y) {
+        dvy -= 1
+    } else if (moon.y < other.y) {
+        dvy += 1
+    }
+
+    if (moon.z > other.z) {
+        dvz -= 1
+    } else if (moon.z < other.z) {
+        dvz += 1
+    }
+
+    return Moon(moon.x, moon.y, moon.z, moon.vx + dvx, moon.vy + dvy, moon.vz + dvz)
 }
 
-fun updatePositions(moons: List<Moon>) = moons.forEach { moon -> moon.position += moon.velocity }
+fun updateVelocity(moon: Moon, others: List<Moon>): Moon {
+    var output = moon
+    others.forEach { output = calculateVelocity(output, it) }
+    return output
+}
+
+fun updatePosition(moon: Moon): Moon =
+    Moon(moon.x + moon.vx, moon.y + moon.vy, moon.z + moon.vz, moon.vx, moon.vy, moon.vz)
+
+fun step(moons: List<Moon>): List<Moon> = moons
+    .map { updateVelocity(it, moons) }
+    .map { updatePosition(it) }
+
+fun part1(moons: List<Moon>): Int = generateSequence(moons) { step(it) }
+    .take(1001)
+    .map{ it.map(Moon::getTotalEnergy).sum() }
+    .last()
 
 fun main() {
     val io = Moon(13, -13, -2)
@@ -71,12 +65,7 @@ fun main() {
     val callisto = Moon(-3, -8, -8)
     val moons = listOf(io, europa, ganymede, callisto)
 
-    (0 until 1000).forEach { _ ->
-        updateVelocities(moons)
-        updatePositions(moons)
-    }
-
-    val totalEnergy = moons.map(Moon::getTotalEnergy).sum()
+    val totalEnergy = part1(moons)
     println("Total energy after 1000 steps: $totalEnergy")
 }
 
