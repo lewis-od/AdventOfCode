b9bd128d4088f6eecc0d62da768a062059e6cb12
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2019-12-17 21:39:39 +0000

    Add potential day 16 pt 2 solution

--- src/uk/co/lewisodriscoll/aoc/day16.kt
+++ src/uk/co/lewisodriscoll/aoc/day16.kt
@@ -29,11 +29,29 @@ fun phase(input: List<Int>): List<Int> = input.indices
     }
     .map { abs(it).getDigit(0) }
 
-fun main() {
-    val input = readFile("day16.txt").first().map(Char::toString).map(String::toInt)
+fun part1(input: List<Int>): List<Int> {
     var output = input
     (1..100).forEach { _ ->
         output = phase(output)
     }
-    println(output.take(8))
+    return output.take(8)
+}
+
+// WARNING: Very very very slow
+fun part2(input: List<Int>): List<Int> {
+    var output = (0..10000).flatMap { input }
+    (1..100).forEach { _ ->
+        output = phase(output)
+    }
+    val offset = input.take(8).mapIndexed { n, x -> 10.pow(n) * x }.sum()
+    return output.subList(offset, offset + 8)
+}
+
+fun main() {
+    val input = readFile("day16.txt").first().map(Char::toString).map(String::toInt)
+    val solution1 = part1(input)
+    println("Solution (part 1): $solution1")
+
+    val solution2 = part2(input)
+    println("Solution (part 1): $solution2")
 }
